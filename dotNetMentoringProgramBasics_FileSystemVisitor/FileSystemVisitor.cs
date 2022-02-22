8ade75014bb75c778a9ca90934d66c9a1e2a397d
Anh Tuan Phan Tran <Anh_Tuan_Phan_Tran@epam.com>
2022-02-22 18:43:14 +0300

    Add project files.

---
+++ dotNetMentoringProgramBasics_FileSystemVisitor/FileSystemVisitor.cs
@@ -0,0 +1,19 @@
+﻿namespace dotNetMentoringProgramBasics_FileSystemVisitor
+{
+    public class FileSystemVisitor
+    {
+        private string _directory;
+        public FileSystemVisitor(string path)
+        {
+            _directory = path;
+        }
+
+        public IEnumerable<string> GetAllPossibleDirectories()
+        {
+            foreach(var dir in Directory.GetDirectories(_directory))
+            {
+                yield return dir;
+            }
+        }
+    }
+}
