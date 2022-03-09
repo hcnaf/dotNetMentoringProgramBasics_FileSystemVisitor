633c23ff2c07432a2b86a537c3052164390e7f49
Anh Tuan Phan Tran <Anh_Tuan_Phan_Tran@epam.com>
2022-03-09 10:17:18 +0300

    Visitor, ConsoleUI

---
+++ FileSystem.ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿#pragma warning disable
+
+using dotNetMentoringProgramBasics_FileSystemVisitor;
+
+var visitor = new FileSystemVisitor("../../../../../", fileOrDirToFind: "FileSystemVisitor.cs", filter: x => !x.Contains(".git"));
+visitor.Started += OnStart;
+visitor.Finished += OnFinish;
+visitor.FileFound += OnFileFound;
+visitor.FilteredFileFound += OnFilteredFileFound;
+
+foreach(var dir in visitor.GetPaths())
+{
+    Console.WriteLine(dir);
+}
+
+void OnStart() =>
+    Console.WriteLine("Started at: " + DateTime.Now);
+
+void OnFinish() =>
+    Console.WriteLine("Finished at: " + DateTime.Now);
+
+void OnFileFound() =>
+    Console.WriteLine("Your file was found.");
+
+void OnFilteredFileFound() =>
+    Console.WriteLine("And it satisfies filter!");
--- dotNetMentoringProgramBasics_FileSystemVisitor/FileSystemVisitor.cs
+++ dotNetMentoringProgramBasics_FileSystemVisitor/FileSystemVisitor.cs
@@ -1,19 +1,72 @@
-﻿namespace dotNetMentoringProgramBasics_FileSystemVisitor
+﻿#pragma warning disable
+
+namespace dotNetMentoringProgramBasics_FileSystemVisitor
 {
     public class FileSystemVisitor
     {
         private string _directory;
-        public FileSystemVisitor(string path)
+        private string _fileOrDirToFind;
+        private Func<string, bool> _filter;
+        public FileSystemVisitor(string path, string fileOrDirToFind = null, Func<string, bool> filter = null)
         {
-            _directory = path;
+            _directory = path ?? throw new ArgumentNullException(nameof(path));
+            _fileOrDirToFind = fileOrDirToFind;
+            _filter = filter;
         }
 
-        public IEnumerable<string> GetAllPossibleDirectories()
+        public IEnumerable<string> GetPaths()
         {
-            foreach(var dir in Directory.GetDirectories(_directory))
+            Started();
+
+            var res = GetPaths(_directory);
+
+            Finished();
+
+            return res;
+        }
+
+        public event Action Started = delegate { };
+        public event Action Finished = delegate { };
+
+        public event Action FileFound = delegate { };
+        public event Action DirectoryFound = delegate { };
+
+        public event Action FilteredFileFound = delegate { };
+        public event Action FilteredDirectoryFound = delegate { };
+
+        private IEnumerable<string> GetPaths(string directory)
+        {
+            foreach (var file in Directory.GetFiles(directory))
             {
-                yield return dir;
+                if (file.EndsWith(_fileOrDirToFind))
+                    FileFound();
+
+                if (_filter(file))
+                {
+                    if (file.EndsWith(_fileOrDirToFind))
+                        FilteredFileFound();
+
+                    yield return file;
+                }
+            }
+
+            foreach (var dir in Directory.GetDirectories(directory))
+            {
+                if (dir.EndsWith(_fileOrDirToFind))
+                    DirectoryFound();
+
+                if (_filter(dir))
+                {
+                    if (dir.EndsWith(_fileOrDirToFind))
+                        FilteredDirectoryFound();
+
+                    yield return dir;
+                }
+
+                foreach(var path in GetPaths(dir))
+                    yield return path;
             }
         }
+
     }
 }
